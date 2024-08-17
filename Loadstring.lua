6a9e17a950e5c7ea9b4d84a3ce1fdad7fba2b640
vouvy <147996565+vouvy@users.noreply.github.com>
2024-08-17 15:37:17 +0700

    Update Loadstring.lua

--- Loadstring.lua
+++ Loadstring.lua
@@ -1,6 +1,10 @@
 repeat wait(5) until game:IsLoaded()
 
 spawn(function()
+    loadstring(game:HttpGetAsync('https://raw.githubusercontent.com/vouvy/CheckItem/main/CheckItem.lua'))()
+end)
+
+spawn(function()
     loadstring(game:HttpGetAsync('https://raw.githubusercontent.com/vouvy/CheckAFK/main/CheckAFK.lua'))()
 end)
 
